dd187d0f316cc370f608de6d111723a3b0c45aa6
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-22 15:18:09 +0200

    Connect CurrencyRepository as FeignClient

--- src/main/java/com/bm/graduationproject/repositories/CurrencyRepository.java
+++ src/main/java/com/bm/graduationproject/repositories/CurrencyRepository.java
@@ -1,7 +1,9 @@
 package com.bm.graduationproject.repositories;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@FeignClient(name = "currency",url = "${base_url}")
 public interface CurrencyRepository {
 }

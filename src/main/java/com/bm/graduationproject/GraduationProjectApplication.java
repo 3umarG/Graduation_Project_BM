15a500dbf0e74d4d6f6720059fe876da410270a4
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-22 15:19:33 +0200

    Enable Feign Clients with the `basePackages` annotation.

--- src/main/java/com/bm/graduationproject/GraduationProjectApplication.java
+++ src/main/java/com/bm/graduationproject/GraduationProjectApplication.java
@@ -1,11 +1,12 @@
 package com.bm.graduationproject;
 
+import com.bm.graduationproject.repositories.CurrencyRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.bm.graduationproject.repositories")
 public class GraduationProjectApplication {
 
     public static void main(String[] args) {

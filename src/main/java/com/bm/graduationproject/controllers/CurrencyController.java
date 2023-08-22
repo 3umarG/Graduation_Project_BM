a8043177205b90a79afdb16eebcfab380671e171
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-22 15:19:00 +0200

    Inject CurrencyService in CurrencyController

--- src/main/java/com/bm/graduationproject/controllers/CurrencyController.java
+++ src/main/java/com/bm/graduationproject/controllers/CurrencyController.java
@@ -1,9 +1,18 @@
 package com.bm.graduationproject.controllers;
 
+import com.bm.graduationproject.services.BaseCurrencyService;
+import com.bm.graduationproject.services.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/currency")
 public class CurrencyController {
+    private BaseCurrencyService service;
+
+    @Autowired
+    public CurrencyController(CurrencyService service){
+        this.service=service;
+    }
 }

ae6cb4f492bd1a53e2e70b0742a659623ce386b1
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-22 15:18:38 +0200

    Inject CurrencyRepository to CurrencyService

--- src/main/java/com/bm/graduationproject/services/CurrencyService.java
+++ src/main/java/com/bm/graduationproject/services/CurrencyService.java
@@ -1,4 +1,16 @@
 package com.bm.graduationproject.services;
 
-public class CurrencyService implements BaseCurrencyService{
+import com.bm.graduationproject.repositories.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrencyService implements BaseCurrencyService {
+
+    private CurrencyRepository repository;
+
+    @Autowired
+    public CurrencyService(CurrencyRepository repository) {
+        this.repository = repository;
+    }
 }

f9ec8df1b8c7408cd23f7a6b25b39a780c91e4b3
MirnaMohamed <mirnaattallah00@gmail.com>
2023-08-25 19:30:41 +0300

    Adding Caching to CurrencyService

--- src/main/java/com/bm/graduationproject/GraduationProjectApplication.java
+++ src/main/java/com/bm/graduationproject/GraduationProjectApplication.java
@@ -3,10 +3,12 @@ package com.bm.graduationproject;
 import com.bm.graduationproject.repositories.CurrencyRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.bm.graduationproject.repositories")
+@EnableCaching
 public class GraduationProjectApplication {
 
     public static void main(String[] args) {

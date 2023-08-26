de53145dc94c8f8acda257c518384cda7da68fe1
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-26 13:48:40 +0200

    Write all Unit Tests of `getExchangeRate` endpoint.

--- src/main/java/com/bm/graduationproject/web/controllers/CurrencyController.java
+++ src/main/java/com/bm/graduationproject/web/controllers/CurrencyController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
@@ -32,7 +33,7 @@ public class CurrencyController {
             , @RequestParam("to1") Currency to1
             , @RequestParam(value = "to2", required = false) Currency to2
             , @RequestParam("amount") @Min(1) Double amount) throws TimeoutException {
-        if (amount<=0)
+        if (amount <= 0)
             throw new NotValidAmountException("Amount must be greater than Zero");
         return ResponseEntity.ok(ApiCustomResponse.builder()
                 .statusCode(200)
@@ -46,19 +47,24 @@ public class CurrencyController {
     @GetMapping()
     public ResponseEntity<ApiCustomResponse<List<CurrencyResponseDto>>> getAllCurrencies() {
         return ResponseEntity.ok(ApiCustomResponse.<List<CurrencyResponseDto>>builder()
-                        .data(service.getAllCurrencies())
-                        .statusCode(200)
-                        .isSuccess(true)
-                        .build());
+                .data(service.getAllCurrencies())
+                .statusCode(200)
+                .isSuccess(true)
+                .build());
     }
 
 
     @PostMapping()
-    public ResponseEntity<ApiCustomResponse<FavoritesResponseDto>> getExchangeRate(@RequestParam(name = "base") Currency baseCurrency, @RequestBody List<Currency> favorites){
+    public ResponseEntity<ApiCustomResponse<FavoritesResponseDto>> getExchangeRate(@RequestParam(name = "base") Currency baseCurrency, @RequestBody List<Currency> favorites) {
         return ResponseEntity.ok(ApiCustomResponse.<FavoritesResponseDto>builder()
-                        .data(service.getExchangeRate(baseCurrency, favorites))
-                        .isSuccess(true)
-                        .statusCode(200)
+                .data(favorites.isEmpty()
+                        ? new FavoritesResponseDto()
+                        : service.getExchangeRate(baseCurrency, favorites))
+                .isSuccess(true)
+                .message(favorites.isEmpty()
+                        ? "There is no Favorites !!"
+                        : null)
+                .statusCode(200)
                 .build());
     }
 }

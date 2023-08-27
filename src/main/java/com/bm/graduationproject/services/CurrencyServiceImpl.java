e4be9924ea8987f4984d6be0d2a46db8da4b9ad8
MirnaMohamed <mirnaattallah00@gmail.com>
2023-08-27 15:48:08 +0300

    Adding logger to CurrencyService methods

--- src/main/java/com/bm/graduationproject/services/CurrencyServiceImpl.java
+++ src/main/java/com/bm/graduationproject/services/CurrencyServiceImpl.java
@@ -8,7 +8,8 @@ import com.bm.graduationproject.models.enums.Currency;
 import com.bm.graduationproject.web.response.ConversionOpenApiResponse;
 import com.bm.graduationproject.models.FavoritesResponseDto;
 import com.bm.graduationproject.repositories.CurrencyRepository;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
@@ -26,6 +27,7 @@ import java.util.stream.Collectors;
 public class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrencyRepository repository;
+    private Logger logger = LoggerFactory.getLogger(CurrencyServiceImpl.class);
     private final ExchangeRateAdapter adapter;
 
     @Autowired
@@ -37,19 +39,23 @@ public class CurrencyServiceImpl implements CurrencyService {
     @Override
     @Cacheable(value = "conversionCache", key = "#from + '-' + #to + '-' + #amount")
     public ConversionResponseDto convert(String from, String to, double amount) {
+        logger.info("Converting from "+ amount +" " + from + " to " + to);
         ConversionOpenApiResponse apiResponse = repository.getCurrencyPair(from, to, amount);
-
         ConversionResponseDto conversionResponseDto = new ConversionResponseDto();
         conversionResponseDto.setSource(from.toUpperCase());
         conversionResponseDto.setDestination(to.toUpperCase());
         conversionResponseDto.setAmount(apiResponse.getConversion_result() == null
                 ? apiResponse.getConversion_rate()
                 : apiResponse.getConversion_result());
+        logger.info("Conversion completed successfully");
+        logger.info("The result is: " + apiResponse.getConversion_result() +" " + to);
         return conversionResponseDto;
     }
 
     @Override
     public List<CurrencyResponseDto> getAllCurrencies() {
+        logger.info("Getting currencies enum");
+        logger.info("Total number of currencies: " + Currency.values().length);
         return Arrays.stream(Currency.values())
                 .map(c -> new CurrencyResponseDto(c.name(), c.getCountry(), c.getFlagImageUrl(), null))
                 .collect(Collectors.toList());
@@ -61,8 +67,11 @@ public class CurrencyServiceImpl implements CurrencyService {
             key = "#src + '-' + #des1 + '-' + #des2 + '-' + #amount")
     public CompareResponseDto compare(String src, String des1, String des2, Double amount) {
         ConversionOpenApiResponse firstConvert = repository.getCurrencyPair(src, des1, amount);
+        logger.info("Converting from "+ amount + " " + src + " to " + des1);
+        logger.info("The result is: " + firstConvert.getConversion_result() +" " + des1);
         ConversionOpenApiResponse secondConvert = repository.getCurrencyPair(src, des2, amount);
-
+        logger.info("Converting from "+ amount + " " + src + " to " + des2);
+        logger.info("The result is: " + secondConvert.getConversion_result() +" " + des2);
         return CompareResponseDto.builder()
                 .source(src)
                 .destination1(firstConvert.getTarget_code())
@@ -83,9 +92,34 @@ public class CurrencyServiceImpl implements CurrencyService {
             value = "exchangeRateCache",
             key ="#baseCurrency.name() + '-' + T(String).join(#favourites.toString())")
     public FavoritesResponseDto getExchangeRate(Currency baseCurrency, List<Currency> favourites) {
+        logger.info("Getting the exchange rate:");
+        String base = baseCurrency.name();
+        logger.info("Base currency: " + base);
+        List<CurrencyResponseDto> currencies = new ArrayList<>();
         ExchangeRateOpenApiResponseDto exchangeRateDto = repository.getExchangeRate(baseCurrency.name());
         return adapter.adapt(exchangeRateDto, baseCurrency, favourites);
+        Map<String, Double> currencies_rate = exchangeRateDto.getConversion_rates();
+        favourites.forEach(f -> {
+            Double currencyRate = getCurrencyValue(currencies_rate, f.name());
+            Currency currency = Currency.valueOf(f.name());
+            logger.info(f.name() + " exchange rate is: " + currencyRate);
+            CurrencyResponseDto currencyInfo = new CurrencyResponseDto(currency.name(), currency.getCountry(),
+                    currency.getFlagImageUrl(), currencyRate);
+            currencies.add(currencyInfo);
+        });
+
+        FavoritesResponseDto response = new FavoritesResponseDto();
+        response.setBase(base);
+        response.setCurrencies(currencies);
+        return response;
     }
 
+    private Double getCurrencyValue(Map<String, Double> currencyRate, String fav) {
+        return currencyRate.entrySet().stream().filter(c -> c.getKey().equals(fav.toUpperCase()))
+                .map(Map.Entry::getValue).toList().get(0);
+    }
 
+    public Double getCurrencyValue(String base, String fav) {
+        return convert(base, fav, 1).getAmount();
+    }
 }
